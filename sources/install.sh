ee74ce2a89814ef95bc9013820f700f5602f5f90
Pradeep <pradeep.jagadeesh@huawei.com>
2015-11-30 15:26:35 +0100

    First internal Commit

---
+++ sources/install.sh
@@ -0,0 +1,9 @@
+sudo yum groupinstall "Development Tools"
+sudo yum install gtk+-devel gtk2-devel
+sudo yum install glib*
+sudo yum -y install readline-devel.x86_64
+
+sudo yum install xorg-x11-xauth
+sudo yum install xorg-x11-fonts-*
+sudo yum install xorg-x11-utils
+
